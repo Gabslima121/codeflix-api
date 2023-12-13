9c310c63993c82bc290bb204197fc0456267fe0b
g.lima2 <g.lima@grouplinknetwork.com>
2023-12-13 19:16:49 +0000

    Refactored category repository to implements CategorySearch params and result

--- src/category/domain/category.repository.ts
+++ src/category/domain/category.repository.ts
@@ -1,7 +1,21 @@
 import { ISearchableRepository } from "../../shared/domain/repository/repositoy-interface";
+import { SearchParams } from "../../shared/domain/repository/search-params";
+import { SearchResult } from "../../shared/domain/repository/search-result";
 import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
 import { Category } from "./category.entity";
 
 //armazenamento only
+export type CategoryFilter = string;
+
+export class CategorySearchParams extends SearchParams<CategoryFilter> {}
+
+export class CategorySearchResult extends SearchResult<Category> {}
+
 export interface ICategoryRepository
-  extends ISearchableRepository<Category, Uuid> {}
+  extends ISearchableRepository<
+    Category,
+    Uuid,
+    CategoryFilter,
+    CategorySearchParams,
+    CategorySearchResult
+  > {}
