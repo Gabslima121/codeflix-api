4c964181893745dd4866b1475856774764cd442d
g.lima2 <g.lima@grouplinknetwork.com>
2023-12-13 18:15:04 +0000

    done category repo part1

--- src/category/domain/category.repository.ts
+++ src/category/domain/category.repository.ts
@@ -1,6 +1,7 @@
-import { IRepository } from "../../shared/domain/repository/repositoy-interface";
+import { ISearchableRepository } from "../../shared/domain/repository/repositoy-interface";
 import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
 import { Category } from "./category.entity";
 
 //armazenamento only
-export interface CategoryRepository extends IRepository<Category, Uuid> {}
+export interface ICategoryRepository
+  extends ISearchableRepository<Category, Uuid> {}
