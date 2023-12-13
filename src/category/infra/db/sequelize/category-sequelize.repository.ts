09256d5af221555e8f0786d283a13b7406d9778f
g.lima2 <g.lima@grouplinknetwork.com>
2023-12-13 19:17:34 +0000

    implemented update, delete, search (with pagination)

--- src/category/infra/db/sequelize/category-sequelize.repository.ts
+++ src/category/infra/db/sequelize/category-sequelize.repository.ts
@@ -1,9 +1,13 @@
-import { Entity } from "../../../../shared/domain/entity";
-import { SearchParams } from "../../../../shared/domain/repository/search-params";
-import { SearchResult } from "../../../../shared/domain/repository/search-result";
+import { Op } from "sequelize";
+
+import { NotFoundError } from "../../../../shared/domain/errors/not-found.error";
 import { Uuid } from "../../../../shared/domain/value-objects/uuid.vo";
 import { Category } from "../../../domain/category.entity";
-import { ICategoryRepository } from "../../../domain/category.repository";
+import {
+  CategorySearchParams,
+  CategorySearchResult,
+  ICategoryRepository,
+} from "../../../domain/category.repository";
 import { CategoryModel } from "./category.model";
 
 export class CategorySequelizeRepository implements ICategoryRepository {
@@ -33,16 +37,35 @@ export class CategorySequelizeRepository implements ICategoryRepository {
     );
   }
 
-  update(entity: Category): Promise<void> {
-    throw new Error("Method not implemented.");
+  async update(entity: Category): Promise<void> {
+    const categoryId = entity.category_id.id;
+    const model = await this._get(categoryId);
+
+    if (!model) throw new NotFoundError(categoryId, this.getEntity());
+
+    await this.categoryModel.update(
+      {
+        category_id: entity.category_id.id,
+        name: entity.name,
+        description: entity.description,
+        is_active: entity.is_active,
+        created_at: entity.created_at,
+      },
+      { where: { category_id: categoryId } }
+    );
   }
 
-  delete(entity_id: Uuid): Promise<void> {
-    throw new Error("Method not implemented.");
+  async delete(entity_id: Uuid): Promise<void> {
+    const categoryId = entity_id.id;
+    const model = await this._get(categoryId);
+
+    if (!model) throw new NotFoundError(categoryId, this.getEntity());
+
+    await this.categoryModel.destroy({ where: { category_id: categoryId } });
   }
 
   async findById(entity_id: Uuid): Promise<Category | null> {
-    const model = await this.categoryModel.findByPk(entity_id.id);
+    const model = await this._get(entity_id.id);
 
     return new Category({
       category_id: new Uuid(model.category_id),
@@ -51,6 +74,10 @@ export class CategorySequelizeRepository implements ICategoryRepository {
       is_active: model.is_active,
       created_at: model.createdAt,
     });
+  }
+
+  private async _get(id: string) {
+    return await this.categoryModel.findByPk(id);
   }
 
   async findAll(): Promise<Category[]> {
@@ -71,7 +98,36 @@ export class CategorySequelizeRepository implements ICategoryRepository {
     return Category;
   }
 
-  search(props: SearchParams<string>): Promise<SearchResult<Entity>> {
-    throw new Error("Method not implemented.");
+  async search(props: CategorySearchParams): Promise<CategorySearchResult> {
+    const offset = (props.page - 1) * props.per_page;
+    const limit = props.per_page;
+
+    const { rows: models, count } = await this.categoryModel.findAndCountAll({
+      ...(props.filter && {
+        where: {
+          name: { [Op.like]: `%${props.filter}%` },
+        },
+      }),
+      ...(props.sort && this.sortableFields.includes(props.sort)
+        ? { order: [[props.sort, props.sort_dir]] }
+        : { order: [["created_at", "desc"]] }),
+      offset,
+      limit,
+    });
+
+    return new CategorySearchResult({
+      items: models.map((model) => {
+        return new Category({
+          category_id: new Uuid(model.category_id),
+          name: model.name,
+          description: model.description,
+          is_active: model.is_active,
+          created_at: model.created_at,
+        });
+      }),
+      current_page: props.page,
+      per_page: props.per_page,
+      total: count,
+    });
   }
 }
