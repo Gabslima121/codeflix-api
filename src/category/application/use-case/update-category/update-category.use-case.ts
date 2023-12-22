bb5228af3743d0d58046f8142f468e49ffde4f69
g.lima2 <g.lima@grouplinknetwork.com>
2023-12-22 12:16:46 +0000

    update category useCase

---
+++ src/category/application/use-case/update-category/update-category.use-case.ts
@@ -0,0 +1,50 @@
+import { IUseCase } from "../../../../shared/application/use-case.interface";
+import { NotFoundError } from "../../../../shared/domain/errors/not-found.error";
+import { Uuid } from "../../../../shared/domain/value-objects/uuid.vo";
+import { Category } from "../../../domain/category.entity";
+import { ICategoryRepository } from "../../../domain/category.repository";
+import { CreateCategoryOutput } from "../create-category/create-category.use-case";
+
+export type UpdateCategoryInput = {
+  id: string;
+  name?: string;
+  description?: string;
+  is_active?: boolean;
+};
+
+export type UpdateCategoryOutput = CreateCategoryOutput;
+
+export class UpdateCategoryUseCase
+  implements IUseCase<UpdateCategoryInput, UpdateCategoryOutput>
+{
+  constructor(private categoryRepository: ICategoryRepository) {}
+
+  async execute(input: UpdateCategoryInput): Promise<UpdateCategoryOutput> {
+    const uuid = new Uuid(input.id);
+
+    const category = await this.categoryRepository.findById(uuid);
+    console.log(category);
+
+    if (!category) throw new NotFoundError(input.id, Category);
+
+    input.name && category.changeName(input.name);
+
+    if ("description" in input) {
+      category.changeDescription(input.description);
+    }
+
+    if (input.is_active === true) category.activate();
+
+    if (input.is_active === false) category.deactivate();
+
+    await this.categoryRepository.update(category);
+
+    return {
+      category_id: category.category_id.id,
+      name: category.name,
+      description: category.description,
+      is_active: category.is_active,
+      created_at: category.created_at,
+    };
+  }
+}
