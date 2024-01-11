403a51a703617e2523283f8bb5dd003684d4f234
g.lima2 <g.lima@grouplinknetwork.com>
2024-01-11 16:59:03 +0000

    added sequelize db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      host: ':memory:',
+      logging: false,
+      models: [CategoryModel],
+    }),
+    SequelizeModule.forFeature(),
+    CategoriesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
