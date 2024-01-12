e64d6369aba0b65ffc260d9e215234e88db84ce9
g.lima2 <g.lima@grouplinknetwork.com>
2024-01-12 14:45:49 +0000

    config module and database module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
-import { SequelizeModule } from '@nestjs/sequelize';
 
-import { CategoryModel } from '@core/category/infra/db/sequelize/category.model';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [DatabaseModule, CategoriesModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
